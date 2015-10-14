22a1caf2bb21c67082aa387a77d309a5d153d246
Kevin French <fnivek@gmail.com>
2015-10-13 22:55:20 -0400

    SNAKE: Make simple snake game

--- game.py
+++ game.py
@@ -1,102 +1,83 @@
 #! /usr/bin/env python
 
 import pygame
-import time
-import random
-from collections import deque
 
-# Some variables
-update_rate = 10
-last_time = time.time()
+class Worm:
+    """ This be my worm!"""
+
+    def __init__(self, surface, x, y, length):
+        self.surface = surface
+        self.x = x
+        self.y = y
+        self.length = length
+        self.dir_x = 0
+        self.dir_y = -1
+        self.body = []
+        self.crashed = False
+        self.lifetime = 0
+
+    def key_event(self, event):
+        """ Handle keyboard events """
+        if event.key == pygame.K_UP and self.dir_y != 1:
+            self.dir_x = 0
+            self.dir_y = -1
+        elif event.key == pygame.K_DOWN and self.dir_y != -1:
+            self.dir_x = 0
+            self.dir_y = 1
+        elif event.key == pygame.K_LEFT and self.dir_x != 1:
+            self.dir_x = -1
+            self.dir_y = 0
+        elif event.key == pygame.K_RIGHT and self.dir_x != -1:
+            self.dir_x = 1
+            self.dir_y = 0
+
+    def move(self):
+        self.x += self.dir_x
+        self.y += self.dir_y
+
+        if (self.x, self.y) in self.body:
+            self.crashed = True
+        else:
+            self.lifetime += 1
+            if not (self.lifetime % 10):
+                self.length += 1
+
+        self.body.insert(0, (self.x, self.y))
+
+        if len(self.body) > self.length:
+            self.body.pop()
+
+    def draw(self):
+        for x, y in self.body:
+            self.surface.set_at((x, y), (255, 255, 255))
+
+
+# Dimensions
 width = 640
 height = 640
-num_lines = 10
-y_line = [height - (height * i / num_lines) for i in range(num_lines)]
-x_line = [width * i / num_lines for i in range(num_lines)]
-mx, my = 0, 0
-running = True
-pixels_to_keep = 1000
-pixels = deque()
 
-# Initilize a surface
 screen = pygame.display.set_mode((width, height))
-
-# Initilize a clock
 clock = pygame.time.Clock()
+running = True
 
-def wrap_color(c):
-    if c >= 256:
-        return 511 - c
-    else:
-        return c
+# Our worm
+w = Worm(screen, width / 2, height / 2, 50)
 
-def invert_color(c):
-    return 255 - c 
-
-def step_x(x):
-    x = x + update_rate * 25.87654 * step
-    if x > width:
-        x = 0
-    return x
-
-def step_y(y):
-    y = y - update_rate * 10.867 * step
-    if y < 0:
-        y = height
-    return y
-    
-
-
-# Infinite loop
+# Main loop
 while running:
-    # Time stuff
-    now = time.time()
-    step = now - last_time
-    last_time = now
+    screen.fill((0, 0, 0))
+    w.move()
+    w.draw()
 
-    # Event handling
+    if w.crashed or w.x <= 0 or w.x >= width - 1 or w.y <= 0 or w.y >= height - 1:
+        print 'Crash!'
+        running = False
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            break
-        elif event.type == pygame.MOUSEMOTION:
-            mx, my = event.pos
+        elif event.type == pygame.KEYDOWN:
+            w.key_event(event)
 
-    if not running:
-        break
-
-    # Logic
-    x_line = map(step_x, x_line)
-    y_line = map(step_y, y_line)
-
-    for i in xrange(100):
-        rgb = [random.randint(0, 255) for x in range(3)]
-        x = random.randint(0, width - 1)
-        y = random.randint(0, height - 1)
-
-        pixels.append([(x, y), rgb])
-        if len(pixels) > pixels_to_keep:
-            pixels.popleft()
-
-    # Graphics
-    color = [int(now * 1) % 512,
-             int(now * 5) % 512,
-             int(now * 10) % 512]
-    color = map(wrap_color, color)
-
-    # Draw backround
-    screen.fill(color)
-
-    # Draw Lines
-    for l in zip(x_line, y_line):
-        pygame.draw.line(screen, map(invert_color, color), (mx, l[1]), (l[0], my))
-
-    # Draw pixels
-    for p in pixels:
-        screen.set_at(p[0], p[1])
-
-    # Flip the screen
     pygame.display.flip()
-
-    # Sleep 1/24 of a second
-    clock.tick(24)
+    clock.tick(240)
