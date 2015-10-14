87ec0acac5f7a97fd5bf56b4642c39ae650696b4
Kevin French <fnivek@gmail.com>
2015-10-13 22:11:02 -0400

    PIXELS: Draw pixels

--- game.py
+++ game.py
@@ -2,6 +2,8 @@
 
 import pygame
 import time
+import random
+from collections import deque
 
 # Some variables
 update_rate = 10
@@ -13,6 +15,8 @@ y_line = [height - (height * i / num_lines) for i in range(num_lines)]
 x_line = [width * i / num_lines for i in range(num_lines)]
 mx, my = 0, 0
 running = True
+pixels_to_keep = 1000
+pixels = deque()
 
 # Initilize a surface
 screen = pygame.display.set_mode((width, height))
@@ -65,15 +69,33 @@ while running:
     x_line = map(step_x, x_line)
     y_line = map(step_y, y_line)
 
+    for i in xrange(100):
+        rgb = [random.randint(0, 255) for x in range(3)]
+        x = random.randint(0, width - 1)
+        y = random.randint(0, height - 1)
+
+        pixels.append([(x, y), rgb])
+        if len(pixels) > pixels_to_keep:
+            pixels.popleft()
+
     # Graphics
     color = [int(now * 1) % 512,
              int(now * 5) % 512,
              int(now * 10) % 512]
     color = map(wrap_color, color)
 
+    # Draw backround
     screen.fill(color)
+
+    # Draw Lines
     for l in zip(x_line, y_line):
         pygame.draw.line(screen, map(invert_color, color), (mx, l[1]), (l[0], my))
+
+    # Draw pixels
+    for p in pixels:
+        screen.set_at(p[0], p[1])
+
+    # Flip the screen
     pygame.display.flip()
 
     # Sleep 1/24 of a second
